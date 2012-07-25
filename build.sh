50a5bce17e35dbe27e998125b001630519c396f1
David Bernal <david@davidthomasbernal.com>
2012-07-25 02:11:23 -0400

    updated build

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ git co gh-pages
 find .  -not -path  '*_site*' -not -path '*.git*' -not -path "." -prune -exec rm -rf {} \;
 mv _site/* .
 rm -rf _site
-rm build.sh
+rm build.sh char.py readme.md
 #find . -name "*.html" -exec sed -i '' -e 's/href="\//href="\/unicodeforus\//g' {} \;
+
+echo "Built site\n"
