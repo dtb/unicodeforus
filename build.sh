2ffd5b1d7332730bb21c669a11ce86d18e858832
David Bernal <david.bernal@gmail.com>
2013-12-27 02:45:47 -0700

    remove symbol file too

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git co gh-pages
 find .  -not -path  '*_site*' -not -path '*.git*' -not -path "." -prune -exec rm -rf {} \;
 mv _site/* .
 rm -rf _site
-rm build.sh char.py readme.md
+rm build.sh char.py readme.md symbols
 #find . -name "*.html" -exec sed -i '' -e 's/href="\//href="\/unicodeforus\//g' {} \;
 
 echo "Built site"
