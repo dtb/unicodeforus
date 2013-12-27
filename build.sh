8a8509d4280f07c3dc54b7d8784d09e34a202a06
David Bernal <david.bernal@gmail.com>
2013-12-27 02:46:19 -0700

    use standard command

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -e
 
-git co master
+git checkout master
 jekyll
-git co gh-pages
+git checkout gh-pages
 find .  -not -path  '*_site*' -not -path '*.git*' -not -path "." -prune -exec rm -rf {} \;
 mv _site/* .
 rm -rf _site
